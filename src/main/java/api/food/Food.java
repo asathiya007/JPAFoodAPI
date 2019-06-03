634b2546d7c8948eb7bd53c690f6c1ecdb3254e6
asathiya007 <akshay.sathiya@gmail.com>
2019-06-03 09:57:26 -0400

    Created entity class

---
+++ src/main/java/api/food/Food.java
@@ -0,0 +1,80 @@
+package api.food;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="foods")
+public class Food {
+	
+	@Id
+	@Column(name="id")
+	private String id;
+	@Column(name="name")
+	private String name;
+	@Column(name="tasty")
+	private boolean tasty;
+	@Column(name="healthy")
+	private boolean healthy;
+	@ManyToOne
+	@Column(name="food_group")
+	private String foodGroupName;
+	
+	public Food() {
+		super();
+	}
+
+	public Food(String id, String name, boolean tasty, boolean healthy, 
+			String foodGroupName) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.tasty = tasty;
+		this.healthy = healthy;
+		this.foodGroupName = foodGroupName;
+	}
+
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public boolean isTasty() {
+		return tasty;
+	}
+	
+	public void setTasty(boolean tasty) {
+		this.tasty = tasty;
+	}
+	
+	public boolean isHealthy() {
+		return healthy;
+	}
+	
+	public void setHealthy(boolean healthy) {
+		this.healthy = healthy;
+	}
+	
+	public String getFoodGroup() {
+		return foodGroupName;
+	}
+	
+	public void setFoodGroup(String foodGroup) {
+		this.foodGroupName = foodGroup;
+	}
+	
+}
